6abf6b66691e0e0330b4e7080c4a6a98a7e2a3a6
Orvian Group BV <bouwproffsnederlandbv@mac-1.home>
2025-12-07 00:58:44 +0100

    Frontend structure reset fix
    Former-commit-id: b879988ee79248575d460fc7c7a134e89b3bf8c0

--- frontend/.next/dev/static/chunks/16043_next_error_606b45a4.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK || (globalThis.TURBOPACK = [])).push([typeof document === "object" ? document.currentScript : undefined,
-"[project]/Desktop/AO_MASTER_FULL_DEPLOY/frontend/node_modules/next/error.js [client] (ecmascript)", ((__turbopack_context__, module, exports) => {
-
-module.exports = __turbopack_context__.r("[project]/Desktop/AO_MASTER_FULL_DEPLOY/frontend/node_modules/next/dist/pages/_error.js [client] (ecmascript)");
-}),
-]);
-
-//# sourceMappingURL=16043_next_error_606b45a4.js.map
